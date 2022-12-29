dac74c9aa748dae7da0e73bde5b0ccd2a0d350b5
veera <veeralavudi@gmail.com>
2022-12-30 00:45:06 +0530

    thrid commit

--- src/main/java/com/cherrypick/githubactions/GithubactionhelloworldApplication.java
+++ src/main/java/com/cherrypick/githubactions/GithubactionhelloworldApplication.java
@@ -3,7 +3,7 @@ package com.cherrypick.githubactions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class GithubactionhelloworldApplication {
 
 	public static void main(String[] args) {
